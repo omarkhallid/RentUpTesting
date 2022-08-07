092e461307b5598b7cc36dd425f30d3c0a17dd38
Tarek Radwan <Tarek Radwan@TAREK-PC>
2022-08-07 10:28:37 +0200

    automated post request

--- test/demo/Tests.java
+++ test/demo/Tests.java
@@ -22,7 +22,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class LoginTest {   
+public class Tests {   
     public String getCellData(int row, int col) throws Exception {
         XSSFSheet ExcelWSheet;
         XSSFWorkbook ExcelWBook;
@@ -85,7 +85,7 @@ public class LoginTest {
     }
     
     @Test
-    public void FirefoxInvalidTest() throws Exception {
+    public void FirefoxInvalidLoginTest() throws Exception {
 
     	String username = getCellData(2, 0);
     	String password = getCellData(2, 1);
@@ -109,7 +109,7 @@ public class LoginTest {
     }
     
     @Test
-    public void FirefoxLoginTest() throws Exception {
+    public void FirefoxValidLoginTest() throws Exception {
         String email = getCellData(1, 0);
     	String password = getCellData(1, 1);
 
@@ -132,7 +132,7 @@ public class LoginTest {
         driver.close();
     }
     @Test
-    public void ChromeInvalidTest() throws Exception {
+    public void ChromeInvalidLoginTest() throws Exception {
 
     	String username = getCellData(2, 0);
     	String password = getCellData(2, 1);
@@ -156,7 +156,7 @@ public class LoginTest {
     }
 
     @Test
-    public void ChromeValidTest() throws Exception {
+    public void ChromeValidLoginTest() throws Exception {
 
     String email = getCellData(1, 0);
     	String password = getCellData(1, 1);
@@ -181,7 +181,7 @@ public class LoginTest {
     }
 
     @Test
-    public void EdgeInvalidTest() throws Exception {
+    public void EdgeInvalidLoginTest() throws Exception {
 
     	String username = getCellData(2, 0);
     	String password = getCellData(2, 1);
@@ -205,7 +205,7 @@ public class LoginTest {
     }
 
     @Test
-    public void EdgeValidTest() throws Exception {
+    public void EdgeValidLoginTest() throws Exception {
 
     String email = getCellData(1, 0);
     	String password = getCellData(1, 1);
@@ -229,6 +229,76 @@ public class LoginTest {
         driver.close();
 
     }
+    
+    
+     @Test
+    public void ChromePostRequestTest() throws Exception {
+
+        String email = getCellData(1, 0);
+    	String password = getCellData(1, 1);
+        String District = getCellData(1,2);
+        String budget = getCellData(1,3);
+        // Set Firefox Web Driver
+        System.setProperty("webdriver.edge.driver", new java.io.File(".").getCanonicalPath()+"\\" + "msedgedriver.exe");
+    	WebDriver driver = new ChromeDriver();
+        driver = login(driver,email,password);
+        Thread.sleep(8000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/header/nav/div/div[3]/a[2]")).click();
+        driver.findElement(By.xpath("//*[@id=\"citySelect\"]/div[1]/span")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"desktop-option-container\"]/div[2]/div[2]/div[1]")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/div[1]/div/div/div[2]/div[1]/div/div/div[2]/div/div[2]/div[1]/span")).click();
+        driver.findElement(By.xpath("//*[@id=\"desktop-option-container\"]/div[1]/input")).click();
+        driver.findElement(By.className("dropdown-search")).sendKeys(District);
+        driver.findElement(By.xpath("//*[@id=\"desktop-option-container\"]/div[2]/div/div[1]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"neighborhoodSelect\"]/div[1]/span")).click();
+         Thread.sleep(4000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div/div/div[2]/div[1]/div/div/div[2]/div/div[3]/div[2]/div[2]/div[1]/div[1]")).click();
+        Thread.sleep(400);
+        driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/div[1]/div/div/div[2]/div[1]/div/div/div[3]/button")).click();
+        Thread.sleep(400);
+        driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/div[1]/div/div/div[2]/div[2]/main/div/div/div[2]/div[1]/div[1]/div[2]/button/div")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/div[1]/div/div/div[2]/div[2]/main/div/div/div[2]/div[1]/div[3]/div/div/span[4]")).click();
+        Thread.sleep(400);
+        driver.findElement(By.xpath("//*[@id=\"datepicker__value_\"]")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div/div/div[2]/div[2]/main/div/div/div[2]/div[2]/div[1]/div/div[2]/div/div/div/div/footer/div/button[1]")).click();
+        Thread.sleep(4000);
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div/div/div[2]/div[2]/main/div/div/div[2]/div[2]/div[2]/div/div[2]/span/fieldset/div/input")).sendKeys(budget);
+        driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/div[1]/div/div/div[2]/div[2]/main/div/div/div[3]/div/div[2]/div/button")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/div[1]/div/div/div[2]/div[3]/div/div[2]/div[1]/main/div/div[1]/div")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/div[1]/div/div/div[2]/div[3]/div/div[2]/div[2]/main/div/div[3]/div/span")).click();
+        Thread.sleep(4000);
+        driver.findElement(By.cssSelector("#__layout > div > div.main-content-container > div > div > div.p-3.post-form-container > div:nth-child(3) > div > div.mt-4.step-content.pt-2.mx-auto.col-lg-6.col-12 > div:nth-child(3) > div > div.col-md-12.col-lg-8 > span > div.counter-input-contianer > button:nth-child(3)")).click();
+    	Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/div[1]/div/div/div[2]/div[3]/div/div[2]/div[4]/main/div/div[2]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/div[1]/div/div/div[2]/div[3]/div/div[3]/div/div[2]/div/button")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/div[1]/div/div/div[2]/div[3]/div/div[5]/button")).click();
+        Thread.sleep(2000);
+        try {
+            // Check whether a certain element appears which confirms that the login was not successful
+            assertEquals(false, driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/div[1]/div/div[2]/div[1]/div/svg")).isDisplayed()); 
+        } catch(NoSuchElementException e){
+            assertTrue(true);
+        }
+        Thread.sleep(2000);
+        
+        // Close the web driver
+        driver.close();
+
+    }
+    
+    
+    
+    
+    
 }
 // testing commits /////
 //test 2//
